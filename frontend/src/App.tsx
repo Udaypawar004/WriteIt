3aca4b407fdbf660ffd9cf177c59de7a789b5e13
Uday Deshmukh <udaypawar004@gmail.com>
2024-11-02 21:09:53 +0530

    intialized frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './Pages/Signup'
+import Signin from './Pages/Signin'
+import Blog from './Pages/Blog'
+import Home from './Pages/Home'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
