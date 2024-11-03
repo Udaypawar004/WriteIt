c7cad69ea36ec8b9eb365ee7f49ed9fee7e3d88d
Uday Deshmukh <udaypawar004@gmail.com>
2024-11-03 14:09:14 +0530

    Added simple Home page

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import NavBar from "./NavBar"
+import "./CSS/home.css"
+import illustration from '/mainIllustration.svg'
 
 const Home = () => {
+  function onChangeHandler() {
+    console.log("Implementing")
+  }
+
   return (
     <div>
-        <div></div>
+        <NavBar />
+
+        <div className='HomeContainer'>
+          <div className='Headings lg:smallHeadings'>  
+            <div className='text-[8rem] text-black font-semibold'>Write It</div>
+            <div className='text-[3rem] text-gray-700 font-medium pl-5'>Where Ideas, <br /> Find Words.</div>
+          </div>
+          <div className='illustrations flex items-center justify-center overflow-hidden'><img className="w-[50rem] h-auto" src={illustration} alt="" /></div>
+        </div>
     </div>
   )
 }
---
+++ frontend/src/Pages/NavBar.tsx
@@ -0,0 +1,20 @@
+import Logo from "/writeIt-Logo.png"
+import './CSS/navCont.css'
+
+const NavBar = () => {
+  return (
+    <div className='navBar '>
+        <div className="section1 background-circle">
+            <div className="Logo p-8"><img className='w-12 h-w-12 rounded-full overflow-hidden' src={Logo} alt="" /></div>
+            <h2 className='nav-text'>WriteIt</h2>
+            <h2 className='nav-text'>About</h2>
+        </div>
+        <div className="section2 background-circle overflow-hidden">
+            <h2 className='signin'>SignIn</h2>
+            <h2 className='signup'>SignUp</h2>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
